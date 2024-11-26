9d9c12be2b6b924f20500b2d5ab4a14953917ba8
Zubairu Awudu <zubairuawudu123@gmail.com>
2024-11-26 09:40:58 -0500

    loopAssignment

--- app.js
+++ app.js
@@ -19,10 +19,29 @@ for (let number = 1; number <= 100; number++){
   }
 } 
 
-// Prime    Time
-
-// declaring n
+// Part 2   (Prime    Time)
 
 
-for( let n = 2; n % 5 ; n --  )
-  console.log(n);
+
+
+let n = 4; 
+let nextPrime = n + 1; 
+
+while (true) {
+  let isPrime = true;
+
+  for (let i = 2; i <= Math.sqrt(nextPrime); i++) {
+    if (nextPrime % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log(nextPrime); 
+    break;
+  }
+  else{
+    console.log(nextPrime++);
+  }
+}
